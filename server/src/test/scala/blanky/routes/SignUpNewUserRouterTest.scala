23575df801904f257d559f26af508705ca32d7fc
vadim-shb <vadim.shabanov@gmail.com>
2015-04-20 13:20:48 +0300

    Refactoring step

--- server/src/test/scala/blanky/routes/SignUpNewUserRouterTest.scala
+++ server/src/test/scala/blanky/routes/SignUpNewUserRouterTest.scala
@@ -1,9 +1,8 @@
 package blanky.routes
 
-import akka.actor.Actor
 import blanky.AppJsonProtocol._
 import blanky.domain.{SignUpUserDto, User}
-import blanky.utils.ActorStub
+import blanky.test_utils.ActorStub
 import org.scalatest.{MustMatchers, WordSpec}
 import spray.httpx.SprayJsonSupport._
 import spray.json._
@@ -11,19 +10,12 @@ import spray.testkit.ScalatestRouteTest
 
 class SignUpNewUserRouterTest extends WordSpec with MustMatchers with ScalatestRouteTest {
 
-  class SignUpActorStub extends Actor {
-
-    def receive = {
-      case _ => sender ! 12L
-    }
-  }
-
   "SignUpNewUserRouter" must {
     "send user data to SignUpActor AND replay user with id as http response" in {
-      // GIVEN user dto to http request
+      // GIVEN user dto from http request
       val userDto = SignUpUserDto("User Name", "user@example.com", "12345", "en")
 
-      // AND signUpActor send back id of saved user
+      // AND signUpActor will response with id of saved user
       val createdUserId = 12L
       val signUpActorStub = ActorStub.stub()
       signUpActorStub.sendBack(createdUserId)
@@ -31,13 +23,13 @@ class SignUpNewUserRouterTest extends WordSpec with MustMatchers with ScalatestR
       // AND router to test
       val underTest = new SignUpNewUserRouter(signUpActorStub.ref).routing
 
-      // WHEN request send to router
+      // WHEN router receives post-request with user dto
       Post("/sign-up", userDto) ~> underTest ~> check {
 
-        // THEN message has been sent to actor
+        // THEN router sends user dto to signUpActor
         signUpActorStub expectMsg userDto
 
-        // AND it responses with data from sign up actor
+        // AND router responses user data with id from signUpActor
         val responseUser = User(id = Some(createdUserId), name = Some(userDto.name), lang = Some(userDto.lang))
         responseAs[String] === responseUser.toJson.prettyPrint
       }
