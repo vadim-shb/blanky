d16f50e3e30c65f3379e9412f4106e067936bb50
vadim-shb <vadim.shabanov@gmail.com>
2015-04-21 17:21:22 +0300

    Refactoring step

--- client/src/components/main/main-controller.js
+++ client/src/components/main/main-controller.js
@@ -3,14 +3,10 @@
 angular.module('webClient').controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 
     var signUpUrl = 'http://localhost:8085/api/sign-up';
-    var newUser = {
-        name: 'Uzzzer',
-        lang: 'en',
-        email: 'ivan@ivanov.com',
-        password: 'qwerty123'
-    };
 
     $scope.signUpUser = function() {
+        var newUser = $scope.user;
+
         $http.post(signUpUrl, newUser).
             success(function(response) {
                 console.log('All OK')
@@ -18,12 +14,5 @@ angular.module('webClient').controller('mainCtrl', ['$scope', '$http', function(
             error(function(response) {
                 console.log('Error')
             });
-        //$http.post(signUpUrl, newUser).
-        //    success(function(response) {
-        //        console.log('All OK')
-        //    }).
-        //    error(function(response) {
-        //        console.log('Error')
-        //    });
     }
 }]);
--- client/test/components/main/main-controller-spec.js
+++ client/test/components/main/main-controller-spec.js
@@ -1,19 +1,42 @@
 describe('main-controller-spec', function() {
     //var $rootScope;
-    var $controller;
+    var $controllers, $httpBackend, $http;
     beforeEach(module('webClient'));
     beforeEach(inject(function($injector) {
         //
         //    $rootScope = $injector.get('$rootScope');
-        $controller = $injector.get('$controller');
+        $controllers = $injector.get('$controller');
+        $httpBackend = $injector.get('$httpBackend');
+        $http = $injector.get('$http');
         //    $scope = $rootScope.$new();
         //
     }));
 
+    afterEach(function() {
+        $httpBackend.flush();
+    });
 
-    it('should initiate with "Thats something" words', function() {
-        var scope = {};
-        $controller('mainCtrl', {$scope: scope});
-        expect(scope.something).toBe("That's something");
+
+    it('should send sign-up request to server', function() {
+        // GIVEN sign-up user data in scope
+        var signUpUser = {
+            name: 'User Name',
+            email: 'someone@example.com',
+            password: 'securePassword',
+            lang: 'en'
+        };
+        var scope = {
+            user: signUpUser
+        };
+
+        //// AND server response for sign-up request
+        $httpBackend.when('POST', 'http://localhost:8085/api/sign-up').respond({});
+
+        // WHEN push sign-up button
+        var controller = $controllers('mainCtrl', {$scope: scope, $http: $http});
+        scope.signUpUser();
+
+        // THEN send sign-up http request
+        $httpBackend.expectPOST('http://localhost:8085/api/sign-up', signUpUser);
     })
 });
